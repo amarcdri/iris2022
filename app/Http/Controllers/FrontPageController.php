dfa92bfba78d3e51ccf15004dab165bb861e90fb
Amar Shukla <amarnath.shukla@cdri.world>
2022-11-16 12:13:57 +0530

    add

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -49,7 +49,7 @@ class FrontPageController extends Controller
    
     public function index()
     {
-        $events=Agenda::where('published','=',1)->where('ctid','=',2)->limit(2)->ORDERBY('startdate','ASC')->get();
+        $events=Agenda::where('published','=',1)->where('ctid','=',2)->limit(2)->ORDERBY('startdate','DESC')->get();
         $iris=Agenda::where('published','=',1)->where('id','=',13)->where('ctid','=',1)->ORDERBY('startdate','ASC')->first();
         $galleries=Gallery::where('published','=',1)->ORDERBY('sequence','ASC')->get();
         return view ('frontend.index', compact('events','iris','galleries'));
