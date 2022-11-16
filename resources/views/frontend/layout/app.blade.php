8a5453a1773c6891c168e70ff33ce0556a0e2f75
rohit <admin@cdri.world>
2022-11-16 12:16:53 +0530

    update

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -82,7 +82,7 @@
            
                 <div class="mobile-nav__container"></div>
                 <!-- /.mobile-nav__container -->
-
+                <a href="https://iris.cdri.world/call-for-proposals-iris" target="_blank" class="donate-btn main-header-one__btn">Call for Proposals<i class="fa fa-arrow-right"></i> </a>
            
                 <div class="mobile-nav__top" style="margin-top:20px;">
                     <div class="mobile-nav__social">
