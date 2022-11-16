88a55d74be650c680634a78fce9e6d9dba17353b
Amar Shukla <amarnath.shukla@cdri.world>
2022-11-16 11:36:42 +0530

    add

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -92,7 +92,7 @@
                 </div>
                 <div class="main-menu-wrapper__right">
                     
-                         <a href="{{route('iris.call.proposal')}}"  target="_blank" class="donate-btn main-header-one__btn">&nbsp;&nbsp;Call for Proposals<i class="fa fa-arrow-right"></i> </a>
+                         <a href="{{route('iris.call.proposal')}}"  target="_blank" class="donate-btn main-header-one__btn">&nbsp;&nbsp;Call for Proposals&nbsp;&nbsp;&nbsp;<i class="fa fa-arrow-right"></i> </a>
                 </div>
             </div>
         </nav>
